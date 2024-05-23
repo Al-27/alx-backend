b5f87114000cb3b58391772fe30e6f2a10c23764
Al_27 <ahmedlaghfour@gmail.com>
2024-05-23 02:23:29 +0100

    0 1 basic an fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,7 +3,7 @@
 """
 BaseCaching = __import__("base_caching").BaseCaching
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
     class doc
     """
@@ -12,12 +12,15 @@ class BasicCache(BaseCaching):
         """func
         """
         super().__init__(*args, **kwargs)
-        self.MAX_ITEMS = 99999
     
     def put(self, key, item):
         """func
         """
         if not (key is None or item is None):
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                fo_key = list(self.cache_data.keys())[0]
+                discarded = self.cache_data.pop(fo_key)
+                print(f"DISCARD: {discarded.key}")
             self.cache_data.update({key,item})
     
     def get(self, key):
