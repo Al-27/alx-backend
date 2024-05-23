07d12eacccff5a8dcdbfbd35b470f19c1d45d340
Al_27 <ahmedlaghfour@gmail.com>
2024-05-23 02:03:16 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Caching module
+"""
+BaseCaching = __import__("base_caching")
+
+class BasicCache(BaseCaching):
+    """
+    class doc
+    """
+    
+    def __init__(self):
+        super()
+        self.MAX_ITEMS = 99999
+    
+    def put(self, key, item):
+        if not (key is None or item is None):
+            self.cache_data.update({key,item})
+    
+    def get(self, key):
+        return self.cache_data.get(key)
