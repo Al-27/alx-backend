e1f29d4605659977ae1fd66343e61292787f007e
Al_27 <ahmedlaghfour@gmail.com>
2024-05-23 02:08:08 +0100

    documentation

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,12 +9,18 @@ class BasicCache(BaseCaching):
     """
     
     def __init__(self):
+        """func
+        """
         super()
         self.MAX_ITEMS = 99999
     
     def put(self, key, item):
+        """func
+        """
         if not (key is None or item is None):
             self.cache_data.update({key,item})
     
     def get(self, key):
+        """func
+        """
         return self.cache_data.get(key)
